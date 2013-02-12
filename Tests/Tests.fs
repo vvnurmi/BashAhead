c799f2e2333e942141bc543101b3d1dcfb46444d
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-02-12 23:01:07 +0200

    Wrote a real test for hero honor accumulation

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,9 +1,51 @@
 ﻿module BashAhead.Tests
 
 open FsCheck
+open FsCheck.Commands
+open FsCheck.Prop
+open Types
+open State
 open Conditions
+open Update
 
-let ``can bounce one step`` monsters = not <| List.isEmpty monsters ==> lazy canBounce monsters
+type HeroHonorModel = Honor * int
+
+let heroHonorSpec =
+    let shiftMatchesAccu accu = function
+        | Honorable, amplitude -> accu = amplitude
+        | Inglorious, amplitude -> accu = -amplitude
+    let postCondition state (honor, accu) =
+        let op = getState <| fun state -> (state.heroHonor, state.heroHonorShift)
+        let (stateHonor, stateHonorShift), _ = rwState.RunOp(op, state)
+        stateHonor = honor && shiftMatchesAccu accu stateHonorShift
+    let honorable =
+        { new ICommand<StateT, HeroHonorModel>() with
+            member x.RunActual state =
+                let op = applyChanges [ HeroHonor(Honorable, 1) ]
+                let _, state2 = rwState.RunOp(op, state)
+                state2
+            member x.RunModel((honor, accu)) =
+                if accu = -1 then honor, 1
+                else if accu < 4 then honor, accu + 1
+                else Honorable, 5
+            member x.Post(state, model) = postCondition state model
+            override x.ToString() = "honorable" }
+    let inglorious =
+        { new ICommand<StateT, HeroHonorModel>() with
+            member x.RunActual state =
+                let op = applyChanges [ HeroHonor(Inglorious, 1) ]
+                let _, state2 = rwState.RunOp(op, state)
+                state2
+            member x.RunModel((honor, accu)) =
+                if accu = 1 then honor, -1
+                else if accu > -4 then honor, accu - 1
+                else Inglorious, -5
+            member x.Post(state, model) = postCondition state model
+            override x.ToString() = "inglorious" }
+    { new ISpecification<StateT, HeroHonorModel> with
+        member x.Initial() = stateUnit, (Honorable, 1)
+        member x.GenCommand _ = Gen.elements [ honorable; inglorious ] }
+let ``hero honor changes`` = asProperty heroHonorSpec
 
 type Marker = class end
 Check.All(Config.Default, typeof<Marker>.DeclaringType)
